5d82651dfa0b5d8c124f4e0542f3a4aa911b6afa
Paymahn <54037788+Paymahn@users.noreply.github.com>
2025-08-05 16:12:46 +0700

    Update convert_flags.py

--- convert_flags.py
+++ convert_flags.py
@@ -12,9 +12,9 @@ for filename in os.listdir(flags_folder):
         destination_path = os.path.join(flags_folder, new_filename)
         try:
             cairosvg.svg2png(url=source_path, write_to=destination_path)
-            print(f"  > Converted {filename} to {new_filename}")
+            print(f"Converted {filename} to {new_filename}")
         except Exception as e:
-            print(f"  > FAILED to convert {filename}: {e}")
+            print(f"FAILED to convert {filename}: {e}")
 
 print("Conversion complete.")
 
